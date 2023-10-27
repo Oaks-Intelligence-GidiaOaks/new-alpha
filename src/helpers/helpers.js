61e4bc615d3d39a94e64f7c1922500b87f0f8bf8
Emmanuel otuonye <emmaotuonye1@gmail.com>
2023-10-27 13:03:04 +0100

    llljj

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,3 +1,3 @@
-export const apiUrl = "https://alpha-scores.onrender.com/api/v1/";
+export const apiUrl = "https://alpha-scores.onrender.com/api/v1/register";
 export const aviationApiUrl = "https://api.aviationstack.com/v1/airports";
 export const airport_api_url = "https://api.api-ninjas.com/v1/airports";
